a21db320232b44bdeee0376fe481a68fc08ad2f4
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2024-12-10 13:35:55 +0100

    Adding the root and the power function

--- src/calculator.js
+++ src/calculator.js
@@ -9,3 +9,7 @@ export const substract = (a, b) => a - b;
 export const isOdd = (a) => a % 2 === 1;
 
 export const isEven = (a) => a % 2 === 0;
+
+export const root = (a) => a ** 0.5;
+
+export const power = (a, b) => a ** b;
--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -2,57 +2,73 @@ import { sum, multiplication, division, modulo, substract, isEven, isOdd } from 
 import { describe, it } from "node:test";
 
 describe("Correct sum", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    if (sum(1, 2) !== 3) {
-      throw new Error("1 + 2 did not equal 3");
-    }
-  });
+    it("adds 1 + 2 to equal 3", () => {
+        if (sum(1, 2) !== 3) {
+            throw new Error("1 + 2 did not equal 3");
+        }
+    });
 });
 
 describe("Correct multiplication", () => {
-  it("multiplies 2 * 3 === 6", () => {
-    if (multiplication(2, 3) !== 6) {
-      throw new Error("2 * 3 did not equal 6");
-    }
-  });
+    it("multiplies 2 * 3 === 6", () => {
+        if (multiplication(2, 3) !== 6) {
+            throw new Error("2 * 3 did not equal 6");
+        }
+    });
 });
 
 describe("Correct division", () => {
-  it("divides 10 / 2 === 5", () => {
-    if (division(10, 2) !== 5) {
-      throw new Error("10 / 2 did not equal 5");
-    }
-  });
+    it("divides 10 / 2 === 5", () => {
+        if (division(10, 2) !== 5) {
+            throw new Error("10 / 2 did not equal 5");
+        }
+    });
 });
 
 describe("Correct modulo", () => {
-  it("modulos 10 % 3 === 1", () => {
-    if (modulo(10, 3) !== 1) {
-      throw new Error("10 % 3 did not equal 1");
-    }
-  });
+    it("modulos 10 % 3 === 1", () => {
+        if (modulo(10, 3) !== 1) {
+            throw new Error("10 % 3 did not equal 1");
+        }
+    });
 });
 
 describe("Correct substract", () => {
-  it("substracts 10 - 3 === 7", () => {
-    if (substract(10, 3) !== 7) {
-      throw new Error("10 - 3 did not equal 7");
-    }
-  });
+    it("substracts 10 - 3 === 7", () => {
+        if (substract(10, 3) !== 7) {
+            throw new Error("10 - 3 did not equal 7");
+        }
+    });
 });
 
 describe("Correct isOdd", () => {
-  it("checks if 3 is odd", () => {
-    if (!isOdd(3)) {
-      throw new Error("3 is odd");
-    }
-  });
+    it("checks if 3 is odd", () => {
+        if (!isOdd(3)) {
+            throw new Error("3 is odd");
+        }
+    });
 });
 
 describe("Correct isEven", () => {
-  it("checks if 4 is even", () => {
-    if (!isEven(4)) {
-      throw new Error("4 is even");
-    }
-  });
+    it("checks if 4 is even", () => {
+        if (!isEven(4)) {
+            throw new Error("4 is even");
+        }
+    });
+});
+
+describe("Correct root", () => {
+    it("finds the square root of 9", () => {
+        if (root(9) !== 3) {
+            throw new Error("The square root of 9 is 3");
+        }
+    });
+});
+
+describe("Correct power", () => {
+    it("finds the power of 2^3", () => {
+        if (power(2, 3) !== 8) {
+            throw new Error("2^3 is 8");
+        }
+    });
 });
